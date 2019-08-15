2767c7283918c0e9a1f4b6e2d9ef30b68ab79307
Sarah Serussi <sarah.serussi@acando.no>
2019-08-15 14:19:04 +0200

    add stuff card

--- src/components/stuff.tsx
+++ src/components/stuff.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import StuffCard from "./stuffCard";
 
 const Stuff: React.FC = () => {
-  return <div>Stuff here</div>;
+  return (
+    <React.Fragment>
+      <StuffCard onClick={() => console.log("stuff 1")}>Stuff 1</StuffCard>
+      <StuffCard>Stuff 2</StuffCard>
+      <StuffCard>Stuff 3</StuffCard>
+    </React.Fragment>
+  );
 };
 
 export default Stuff;
---
+++ src/components/stuffCard.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import "../scss/stuff-card.scss";
+import { CardActionArea } from "@material-ui/core";
+
+interface StuffCardProps {
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+const StuffCard: React.FC<StuffCardProps> = ({ children, onClick }) => {
+  return (
+    <Card className="stuff-card">
+      <CardActionArea onClick={onClick}>
+        <CardContent>{children}</CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default StuffCard;
